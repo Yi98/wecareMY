ad568214060cb23123c5f53fd25faf0a89cef641
Yi98 <ngyi07285@hotmail.com>
2020-05-19 14:00:27 +0800

    added express to package.json

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  outputDir: "../server/dist",
+  assetsDir: "static", // relative to outputDir
+  devServer: {
+    host: 'localhost'
+  }
 }
